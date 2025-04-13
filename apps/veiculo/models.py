2e3fa890bc6c74733863070af074f4296503a61c
Guilherme de Jesus Carvalho <82466082+Jesus-jpg1@users.noreply.github.com>
2025-04-12 20:10:32 -0500

    feat: adiciona validação e valor padrão para placa no modelo Veiculo
    - Atualizado o modelo `Veiculo` para incluir validação personalizada no campo `placa` usando o validador `validar_placa`.
    - Configurado o valor padrão do campo `placa` para ser gerado automaticamente no formato 'mercosul' utilizando a função `gerar_placa`.
    
    Essas alterações garantem que as placas sejam validadas e geradas corretamente no modelo Veiculo.

--- apps/veiculo/models.py
+++ apps/veiculo/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from apps.cliente.models import Cliente
+from apps.core.validators import validar_placa
+from apps.core.utils import gerar_placa
 
 
 class Veiculo(models.Model):
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, related_name='veiculos')
     marca = models.CharField(max_length=50)
     modelo = models.CharField(max_length=50)
-    placa = models.CharField(max_length=8)
+    placa = models.CharField(max_length=8, validators=[validar_placa], default=lambda: gerar_placa('mercosul'))
     ano = models.IntegerField()
